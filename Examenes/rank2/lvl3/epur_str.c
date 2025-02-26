3d41a25affaa16e6f909fbbe8a67090c59645b9a
pescriva <pabloroteescriva@gmail.com>
2025-02-26 15:57:18 +0100

    epur_str.c

---
+++ Examenes/rank2/lvl3/epur_str.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+int is_space(char c)
+{
+    if(c <= 32)
+        return 1;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    int space = 0;
+
+    if(argc == 2)
+    {
+        while(is_space(argv[1][i]))
+            i++;
+        while(argv[1][i])
+        {
+            if(is_space(argv[1][i]))
+                space = 1;
+            if(!is_space(argv[1][i]))
+            {
+                if(space)
+                    write(1, " ", 1);
+                space = 0;
+                write(1, &argv[1][i], 1);
+            }
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return(0);
+}
